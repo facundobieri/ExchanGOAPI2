10b1cc0dad7b9c34f8160559d20fc989e8b4dcfa
Facundo Bieri <fbieri@mail.austral.edu.ar>
2026-02-19 21:26:26 -0300

    CORS implemented

--- ExchanGOAPI2/Program.cs
+++ ExchanGOAPI2/Program.cs
@@ -14,6 +14,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AngularApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
 // Database
 builder.Services.AddDbContext<ExchanGODbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -68,6 +80,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("AngularApp");
 app.UseAuthentication();
 app.UseAuthorization();
 
